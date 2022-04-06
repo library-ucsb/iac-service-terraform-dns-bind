d601d7f5581693143a45d7de8ad591bbce46606a
Josh Preston <joshuapreston@ucsb.edu>
2022-04-06 14:03:28 -0700

    added current user to push allow

--- main.tf
+++ main.tf
@@ -1,31 +1,37 @@
+data "github_user" "current" {
+    username = ""
+}
+
 
 # create one unit of `library-workspace`
 module "library_workspace" {
-    source                  = "app.terraform.io/library-ucsb-core/module-library_workspace/tfc"
+    source                          = "app.terraform.io/library-ucsb-core/module-library_workspace/tfc"
 
-    name                    = var.name
-    github_repo_visibility  = var.visibility
-    github_organization     = var.github_organization
-    tfc_organization        = var.tfc_organization
-    description             = var.description
-    oauth_client_id         = var.oauth_client_id
+    name                            = var.name
+    github_repo_visibility          = var.visibility
+    github_organization             = var.github_organization
+    tfc_organization                = var.tfc_organization
+    description                     = var.description
+    oauth_client_id                 = var.oauth_client_id
 
-    github_enforce_admins   = var.github_enforce_admins
+    github_enforce_admins           = var.github_enforce_admins
+    github_repo_push_restrictions   =  [ data.github_user.current.node_id ]
 }
 
 output "github_repo" {
     value = {
-        full_name           = module.library_workspace.github_repo_full_name  
-        ssh_clone_url       = module.library_workspace.github_repo_ssh_clone_url
-        id                  = module.library_workspace.github_repo_id
-        branches            = module.library_workspace.github_repo_branches
+        full_name                   = module.library_workspace.github_repo_full_name  
+        ssh_clone_url               = module.library_workspace.github_repo_ssh_clone_url
+        id                          = module.library_workspace.github_repo_id
+        branches                    = module.library_workspace.github_repo_branches
+        push_restrictions           = module.library_workspace.github_repo_push_restrictions
     }
 }
 
 output "tfc_workspace" {
     value = {
-        id                  = module.library_workspace.tfc_workspace_id
-        name                = module.library_workspace.tfc_workspace_name
-        working_directory   = module.library_workspace.tfc_workspace_working_directory
+        id                          = module.library_workspace.tfc_workspace_id
+        name                        = module.library_workspace.tfc_workspace_name
+        working_directory           = module.library_workspace.tfc_workspace_working_directory
     }
 }
