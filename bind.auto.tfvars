0efb8ea6f358563f534ffca9cf89bff712e8a5ad
Josh Preston <joshuapreston@ucsb.edu>
2022-04-06 13:06:38 -0700

    backed out of tfc to re-deploy fresh

--- bind.auto.tfvars
+++ bind.auto.tfvars
@@ -6,4 +6,3 @@ tfc_organization        = "library-ucsb-core"
 # true = admins must go through checks / false = admins avoid checks
 github_enforce_admins   = false
 
-## test
--- main.tf
+++ main.tf
@@ -12,3 +12,20 @@ module "library_workspace" {
 
     github_enforce_admins   = var.github_enforce_admins
 }
+
+output "github_repo" {
+    value = {
+        full_name           = module.library_workspace.github_repo_full_name  
+        ssh_clone_url       = module.library_workspace.github_repo_ssh_clone_url
+        id                  = module.library_workspace.github_repo_id
+        branches            = module.library_workspace.github_repo_branches
+    }
+}
+
+output "tfc_workspace" {
+    value = {
+        id                  = module.library_workspace.tfc_workspace_id
+        name                = module.library_workspace.tfc_workspace_name
+        working_directory   = module.library_workspace.tfc_workspace_working_directory
+    }
+}
