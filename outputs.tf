38c12c7ee070407548a04de7046dedbebbfdaf9b
Josh Preston <joshuapreston@ucsb.edu>
2022-04-07 15:22:25 -0700

    Added some outputs

--- outputs.tf
+++ outputs.tf
@@ -15,3 +15,23 @@ output "tfc_workspace" {
         working_directory           = module.library_workspace.tfc_workspace_working_directory
     }
 }
+
+output "github_repo_bind_zones" {
+    value = {
+        full_name                   = module.github-repo-bind.full_name
+        node_id                     = module.github-repo-bind.node_id
+        repo_id                     = module.github-repo-bind.repo_id
+        ssh_clone_url               = module.github-repo-bind.ssh_clone_url
+        branches                    = module.github-repo-bind.branches
+    }
+}
+
+output "github_branch_protection-bind_zones" {
+    value = {
+        repo_id                     = module.github-branch_protection-bind.repo_id
+        pattern                     = module.github-branch_protection-bind.pattern
+        enforce_admins              = module.github-branch_protection-bind.enforce_admins
+        push_restrictions           = module.github-branch_protection-bind.push_restrictions
+        allows_force_pushes         = module.github-branch_protection-bind.allows_force_pushes
+    }
+}
